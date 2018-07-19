1c503c2ba223c445e534a221870bdafc19eeafa7
ALM <ALM@dhcp-v074-227.mobile.uci.edu>
2018-07-19 14:33:23 -0700

    Push up crap

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Button from '@material-ui/core/Button';
-import Toolbar from '@material-ui/core/Toolbar';
+import Drawer from '@material-ui/core/Drawer';
+import Button from '@material-ui/core/Button'
+import AppBar from '@material-ui/core/AppBar';
 
 class App extends Component {
   render() {
+    const drawerSettings = {
+      isOpen: true
+    }
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <Button variant="contained" color="primary">
-          Hello World
+        <AppBar position="fixed">
+          ASdasd
+        </AppBar>
+      <Drawer
+        anchor="right"
+        open={drawerSettings.isOpen}>
+        <Button>
+          test
         </Button>
-        <Toolbar />
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+    </Drawer>
       </div>
     );
   }
