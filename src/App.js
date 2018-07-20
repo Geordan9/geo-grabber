161277a64b5241edebbc100a6701b270876e1e4a
ALM <ALM@dhcp-v074-227.mobile.uci.edu>
2018-07-19 21:28:32 -0700

    add bass css

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+
+import { Button } from 'rmwc/Button';
+import { Grid, GridCell, GridInner } from 'rmwc/Grid';
+
+{/* Standard Grid. For convenience, GridInner is added for you */}
+<Grid>
+  <GridCell span="4">1</GridCell>
+  <GridCell span="4">2</GridCell>
+  <GridCell span="4">3</GridCell>
+</Grid>
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Grid>
+        <GridCell className="flex justify-center" span="4"><Button>Hello World</Button></GridCell>
+        <GridCell className="flex justify-center" span="4"><Button>Hello World</Button></GridCell>
+        <GridCell className="flex justify-center" span="4"><Button>Hello World</Button></GridCell>
+      </Grid>
     );
   }
 }
